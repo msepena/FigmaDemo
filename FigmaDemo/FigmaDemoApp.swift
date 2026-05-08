1b4178bd6550ed9f06caaa69d7a3b78b0cb03387
Murali Sepna <sepena4mobile@gmail.com>
2026-05-08 16:53:20 -0700

    Initial Commit

---
+++ FigmaDemo/FigmaDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaDemoApp.swift
+//  FigmaDemo
+//
+//  Created by Hema Sepena on 5/8/26.
+//
+
+import SwiftUI
+
+@main
+struct FigmaDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FigmaDemoTests/FigmaDemoTests.swift
@@ -0,0 +1,19 @@
+//
+//  FigmaDemoTests.swift
+//  FigmaDemoTests
+//
+//  Created by Hema Sepena on 5/8/26.
+//
+
+import Testing
+@testable import FigmaDemo
+
+struct FigmaDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
